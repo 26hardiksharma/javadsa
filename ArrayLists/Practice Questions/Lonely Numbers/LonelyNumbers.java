4a51dfc7d7db4c4db0ac383097f44e7983bf9cb7
Eternal Slayer <74042237+EternalSlayer@users.noreply.github.com>
2025-07-22 18:25:22 +0530

    Updated LonelyNumbers.java

---
+++ ArrayLists/Practice Questions/Lonely Numbers/LonelyNumbers.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class LonelyNumbers {
+
+    public static void lonelyNumbers(ArrayList<Integer> list ) {
+
+
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        int len = list.size();
+        Collections.sort(list);
+        for(int i = 0;i<len;i++) {
+            if(i == 0) {
+                if(!(list.get(0) == list.get(1) || list.get(0) == list.get(1)-1)) {
+                    ans.add(list.get(i));
+                } 
+            } else if(i == len-1) {
+                if(!(list.get(len-1) == list.get(len-2) || list.get(len-1) == list.get(len-2)+1)) {
+                    ans.add(list.get(i));
+                } 
+
+            } else {
+                if(!((list.get(i) == list.get(i+1) || list.get(i) == list.get(i+1)-1) || ((list.get(i) == list.get(i-1) || list.get(i) == list.get(i-1)+1)))) {
+                    ans.add(list.get(i));
+                }
+            }
+        }
+
+        System.out.println(ans);
+        
+        // int n = list.size();
+        // int elem;
+        // boolean flag = false;
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // for(int i =0 ;i<n;i++) {
+        //     elem = list.get(i);
+        //     if(list.contains(elem+1) || list.contains(elem-1)) {
+        //         continue;
+        //     } 
+
+        //     for(int j = 0;j<n;j++) {
+        //         if(list.get(j) == elem && j!=i) {
+        //             flag = true;
+        //             break;
+        //         }
+        //     }
+
+        //     if(flag == false) {
+        //         ans.add(elem);
+
+        //     }
+
+        //     flag = false;
+        // }
+
+        // System.out.println(ans);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(6);
+        list.add(5);
+        list.add(8);
+        lonelyNumbers(list);
+    }    
+}
