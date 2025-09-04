9b6edb9d5af6f4a28e1848fefbabe38c9a8ebe8d
EternalSlayer <hardikslayer2005@gmail.com>
2025-09-04 11:23:18 +0530

    Added QueueReverse.java

---
+++ Queues/QueueReverse.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+public class QueueReverse {
+    public static void reverse(Queue<Integer> q) {
+        int size = q.size();
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0;i<size;i++) {
+            s.push(q.remove());
+        }
+
+        while(!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        reverse(q);
+        while (!q.isEmpty()) {
+            System.out.println(q.remove());
+        }
+    }
+}
