80071e88a8a9781e683e472b6c4230b5fe858192
EternalSlayer <hardikslayer2005@gmail.com>
2025-12-06 03:11:21 +0530

    Add Graphs

---
+++ Graphs/Edge.java
@@ -0,0 +1,12 @@
+public class Edge {
+    
+    int src;
+    int dest;
+    int weight;
+
+    public Edge(int s,int d,int w) {
+        this.src = s;
+        this.dest = d;
+        this.weight = w;
+    }
+}
