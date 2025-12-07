94ad423f555c5cb64db9fb97440ae0e070680383
EternalSlayer <hardikslayer2005@gmail.com>
2025-12-07 18:18:51 +0530

    Add graphs

--- Graphs/ConnectedComponents.java
+++ Graphs/ConnectedComponents.java
@@ -1,4 +1,53 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 public class ConnectedComponents {
+
+    public void bfsUtil(ArrayList<Edge>[] graph,int start,boolean[] vis) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        vis[start] = true;
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if(!vis[curr]) {
+                System.out.println(curr + " ");
+                for(int i = 0;i<graph[curr].size();i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+
+                }
+            }
+        }
+    }
+
+    public void bfs(ArrayList<Edge>[] graph) {
+        boolean[] vis = new boolean[graph.length];
+        for(int i = 0;i<graph.length;i++) {
+            if(!vis[i]) {
+                bfsUtil(graph, i,vis);
+            }
+        }
+    }
+    public void dfs(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        for(int i = 0 ;i<graph.length;i++) {
+            if(!vis[i]) {
+                dfsUtil(graph,i,vis);
+            }
+        }
+    }
+
+    public void dfsUtil(ArrayList<Edge>[] graph,int curr,boolean vis[]) {
+        System.out.println(curr + " ");
+        vis[curr] = true;
+
+        for(int i = 0;i<graph[curr].size();i++) {
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]) {
+                dfsUtil(graph, e.dest, vis);
+            }
+        }
+    }
     public static void main(String[] args) {
         
     }
--- Graphs/Edge.java
+++ Graphs/Edge.java
@@ -54,7 +54,31 @@ public class Edge {
             }
         }
     }
+    public static boolean detectCycle(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        for(int i = 0;i<graph.length;i++) {
+            if(!vis[i]) {
+                if(detectCycleUtil(graph,vis,i,-1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[] vis,int curr,int par) {
+        vis[curr] = true;
+        for(int i = 0;i<graph[curr].size();i++) {
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest] && detectCycleUtil(graph, vis, e.dest, curr)) {
+                return true;
+            } else if(vis[e.dest] && e.dest!=par) {
+                return true;
+            }
+        }
+
+        return false;
+    }
     public static void main(String[] args) {
         int v = 5;
         ArrayList<Edge>[] graph = new ArrayList[v];
@@ -85,4 +109,6 @@ public class Edge {
 
         dfs(graph);
     }   
+
+    
 }
