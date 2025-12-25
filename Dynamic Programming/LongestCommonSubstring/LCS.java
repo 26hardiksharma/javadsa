7328834225c415a3a5f97aa7953fd7e0658c709c
EternalSlayer <hardikslayer2005@gmail.com>
2025-12-25 10:53:26 +0530

    Add LCS

---
+++ Dynamic Programming/LongestCommonSubstring/LCS.java
@@ -0,0 +1,13 @@
+public class LCS {
+
+    public static String lcs(String s1,String s2) {
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        
+    }
+    public static void main(String[] args) {
+        String s1 = "ABCDE";
+        String s2 = "ABGCE";
+
+        
+    }
+}
